70368ab8c9dd7a70d9da87406a65a54aa0f78adf
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2020-02-12 20:15:12 +0100

    NT 2 - Hálózat 19/20 (szomszédlista és halmaz)

--- NT-2020/1-Halozati-biztonsag/halozat.py
+++ NT-2020/1-Halozati-biztonsag/halozat.py
@@ -1,23 +1,28 @@
 from sys import stdin, stdout
 
 def main():
-    n, m, k = map(int, stdin.readline().split())
+    n, _, k = map(int, stdin.readline().split())
     l = [[] for _ in range(n)]
 
-    for _ in range(m):
-        a, b = map(int, stdin.readline().split())
-        l[a-1].append(b-1)
-        l[b-1].append(a-1)
+    be = stdin.readlines()
+    for sor in be:
+        a, b = map(int, sor.split())
+        a -= 1
+        b -= 1
+        l[a].append(b)
+        l[b].append(a)
 
-    rosszak = [i for i, c in enumerate(l) if len(c) < k]
+    rosszak = {i for i, c in enumerate(l) if len(c) < k}
     while rosszak:
         for i, c in enumerate(l):
             if c is not None:
                 if len(c) < k:
                     l[i] = None
-                elif c:
-                    l[i] = [x for x in c if x not in rosszak]
-        rosszak = [i for i, c in enumerate(l) if c is not None and len(c) < k]
+                else:
+                    # l[i] = [x for x in c if x not in rosszak]
+                    # l[i] = tuple(filter(lambda x: x not in rosszak, l[i]))
+                    l[i] = tuple(x for x in c if x not in rosszak)
+        rosszak = {i for i, c in enumerate(l) if c is not None and len(c) < k}
 
     jok = [str(i+1) for i in range(n) if l[i]]
     stdout.write(str(len(jok)))
