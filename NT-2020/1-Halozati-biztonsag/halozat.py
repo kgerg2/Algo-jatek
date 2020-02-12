eec17736701a8ba2cb868baf85152ec2211daf13
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2020-02-12 16:34:57 +0100

    NT 2 - Hálózat 12/20 (teljesítményjavítással)

--- NT-2020/1-Halozati-biztonsag/halozat.py
+++ NT-2020/1-Halozati-biztonsag/halozat.py
@@ -1,20 +1,27 @@
-n, m, k = map(int, input().split())
-l = [set() for _ in range(n)]
+from sys import stdin, stdout
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    l[a-1].add(b-1)
-    l[b-1].add(a-1)
+def main():
+    n, m, k = map(int, stdin.readline().split())
+    l = [set() for _ in range(n)]
 
-rosszak = {i for i, c in enumerate(l) if len(c) < k}
-while rosszak:
-    for i, c in enumerate(l):
-        if i in rosszak:
-            l[i] = None
-        if c:
-            c -= rosszak
-    rosszak = {i for i, c in enumerate(l) if c is not None and len(c) < k}
+    for _ in range(m):
+        a, b = map(int, stdin.readline().split())
+        l[a-1].add(b-1)
+        l[b-1].add(a-1)
 
-jok = [str(i+1) for i in range(n) if l[i]]
-print(len(jok))
-print(" ".join(jok))
+    rosszak = {i for i, c in enumerate(l) if len(c) < k}
+    while rosszak:
+        for i, c in enumerate(l):
+            if i in rosszak:
+                l[i] = None
+            if c:
+                c -= rosszak
+        rosszak = {i for i, c in enumerate(l) if c is not None and len(c) < k}
+
+    jok = [str(i+1) for i in range(n) if l[i]]
+    stdout.write(str(len(jok)))
+    stdout.write("\n")
+    stdout.write(" ".join(jok))
+    stdout.write("\n")
+
+main()
