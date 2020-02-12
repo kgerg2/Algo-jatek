82586526cc8c8c7bca4a8a7c7a301db6bd971745
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2020-02-12 17:04:49 +0100

    NT 2 - Hálózat 13/20 (csúcslista listában)

--- NT-2020/1-Halozati-biztonsag/halozat.py
+++ NT-2020/1-Halozati-biztonsag/halozat.py
@@ -2,25 +2,24 @@ from sys import stdin, stdout
 
 def main():
     n, m, k = map(int, stdin.readline().split())
-    l = [[False]*n for _ in range(n)]
+    l = [[] for _ in range(n)]
 
     for _ in range(m):
         a, b = map(int, stdin.readline().split())
-        l[a-1][b-1] = True
-        l[b-1][a-1] = True
+        l[a-1].append(b-1)
+        l[b-1].append(a-1)
 
-    ures = [False]*n
-    van = True
-    while van:
-        van = False
+    rosszak = [i for i, c in enumerate(l) if len(c) < k]
+    while rosszak:
         for i, c in enumerate(l):
-            if c is not ures and sum(c) < k:
-                l[i] = ures
-                for j in range(n):
-                    l[j][i] = False
-                van = True
+            if c is not None:
+                if len(c) < k:
+                    l[i] = None
+                elif c:
+                    l[i] = [x for x in c if x not in rosszak]
+        rosszak = [i for i, c in enumerate(l) if c is not None and len(c) < k]
 
-    jok = [str(i+1) for i in range(n) if l[i] is not ures]
+    jok = [str(i+1) for i in range(n) if l[i]]
     stdout.write(str(len(jok)))
     stdout.write("\n")
     stdout.write(" ".join(jok))
