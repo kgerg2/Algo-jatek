e29fb1942a817cfd846517ddd5165fd729f7afcf
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2020-04-21 15:33:08 +0200

    Mester - Nem kifizethető címlet (100/100)

--- Mester/Osszefuggo-halozat/osszefuggo-halozat.cpp
+++ Mester/Osszefuggo-halozat/osszefuggo-halozat.cpp
@@ -3,48 +3,44 @@
 
 using namespace std;
 
-vector<vector<int>> G;
-vector<bool> visited;
+vector<vector<int>> G; // gráf éllistás ábrázolással
+vector<bool> visited; // már bejárt csúcsok tárolása
 
-void DFS(int p)
-{
+// Mélységi bejárás
+void DFS(int p) {
     visited[p] = true;
     for (int v : G[p])
-    {
         if (!visited[v])
-        {
             DFS(v);
-        }
-    }
 }
 
-int main()
-{
+int main() {
     int n, m, a, b;
+    
+    // n - csúcsok száma, m - élek száma
     cin >> n >> m;
+
     G.resize(n);
     visited.resize(n, false);
-    for (int i = 0; i < m; i++)
-    {
+
+    for (int i = 0; i < m; i++) {
         cin >> a >> b;
         G[a - 1].push_back(b - 1);
         G[b - 1].push_back(a - 1);
     }
+
+    // komponensek egy-egy csúcsának tárolása
     vector<int> komp;
-    int i = 0;
-    for (int j = 0; j < n; j++)
-    {
-        if (!visited[j])
-        {
+    for (int j = 0; j < n; j++) {
+        if (!visited[j]) {
             komp.push_back(j);
             DFS(j);
-            i++;
         }
     }
-    cout << i - 1 << endl;
-    for (int j = 1; j < i; j++)
-    {
-        cout << komp[0] + 1 << " " << komp[j] + 1 << endl;
-    }
+
+    cout << komp.size() - 1 << endl;
+    for (int i = 1; i < komp.size(); i++)
+        cout << komp[0] + 1 << " " << komp[i] + 1 << endl;
+
     return 0;
 }
